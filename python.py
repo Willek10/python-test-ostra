af123ef6f0d5f6adafcc5e8447339bd380006a59
William Kassmo <166232656+Willek10@users.noreply.github.com>
2025-11-20 10:06:59 +0000

    Added a random hex generator

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import random
+
+letters = "0123456789abcdef"
+hexColor = ["#"]
+
+for i in range(6):
+    hexColor.append(letters[random.randint(0,15)])
+
+print(f"Hex colour: {"".join(hexColor)}")
